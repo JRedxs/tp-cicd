cd2d125f72fcc71437645356425c460ff6da26ef
JRedxs <jredxs@gmail.com>
2024-04-18 09:19:01 +0200

    release

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "CI/CD rocks! Jenkins!" );
+        System.out.println( "CI/CD rocks! Jenkins! Release TOP" );
     }
 }
